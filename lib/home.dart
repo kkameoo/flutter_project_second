3ac122abd188f2da5e90d991777f8e976b291f82
jensen-kor <lukawa1@naver.com>
2025-03-12 10:30:17 +0900

    (경빈) 홈 화면 꾸미기

--- lib/home.dart
+++ lib/home.dart
@@ -25,13 +25,58 @@ class _HomeForm extends StatefulWidget {
 class _HomeFormState extends State<_HomeForm> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: IconButton(
-        icon: Icon(Icons.add),
-        onPressed: () {
-          Navigator.pushNamed(context, "/list");
-        },
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, // 위쪽과 아래쪽에 공간을 자동으로 배분
+        children: [
+          Expanded(child: Container()),  // 화면 상단의 여백을 만들어줍니다.
+
+          // 버튼을 화면 하단에 배치
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,  // 버튼을 중앙 정렬
+            children: [
+              Container(
+                width: 200,  // 네모의 가로 크기
+                height: 200,  // 네모의 세로 크기
+                decoration: BoxDecoration(
+                  color: Colors.blue,  // 배경색 추가
+                  image: DecorationImage(
+                    image: AssetImage('../android/img/다운로드.jpg'),  // 이미지 경로
+                    fit: BoxFit.cover,  // 이미지를 네모 상자에 맞게 꽉 채움
+                  ),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, "/list");
+                  },
+                ),
+              ),
+              SizedBox(width: 10),  // 두 버튼 사이의 간격
+              Container(
+                width: 200,  // 네모의 가로 크기
+                height: 200,  // 네모의 세로 크기
+                decoration: BoxDecoration(
+                  color: Colors.red,  // 배경색 추가
+                  image: DecorationImage(
+                    image: AssetImage('assets/your_image2.png'),  // 다른 이미지 경로
+                    fit: BoxFit.cover,  // 이미지를 네모 상자에 맞게 꽉 채움
+                  ),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, "/remove");
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
+
+
+
+
